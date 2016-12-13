82d9cf0ba8e274a9c7cb069f012c27d58e89eebb
Big XiXi <xixi@bigxixi.com>
2016-12-13 13:35:03 +0800

    Add files via upload

---
+++ Sketch2AE.sketchplugin/Contents/Resources/Localization.js
@@ -0,0 +1,44 @@
+var local_saveDoc = 'Please save your sketch file first!';
+var local_warning = 'Warning!';
+var local_tooMuchArtboards = 'You have to many artboards in this file, which might lead to UI crash.';
+var local_pickArtboardDsp = 'Pick Artboards(current page:';
+var local_exportDsp = 'Piked Artboards will get a snapshot for reference.';
+var local_inputLength = 'The length of the composition(in seconds):';
+var local_inputFPS = 'The FPS of the composition:';
+var local_noArtboards = 'There are no Artboards in this page:(';
+var local_done = "Done, would you like to clean the 'ExportSlice's?";
+var local_clsBtn = "Clean";
+var local_notNowBtn = "Not Now";
+var local_noArtboardExpoted = 'Nothing selected.Nothing exported.';
+var local_chooseTips = "Please select at least 1 layer(group).";
+var local_groupTips = "Group them first if you want to export a non-group layer.";
+var local_helpText = 'How to use：\n' +
+                    '1.Select the layergroups your want to export on the layer list'+
+                    '(group it if the layer is not a layergroup)\n'+
+                    'Run Plugins -> Sketch2AE -> Generate ExportSlices。\n' +
+                    '2.Run Plugins -> Sketch2AE -> Export Artboards, then on the popup panel set the composition length and FPS, pick artboards you want to export. \n'+
+                    'Hit OK and choose a location to save the generated ".jsx" script file and "slice" folder.\n' +
+                    '3.In After Effect，run File -> Script -> Run Script File, choose the ".jsx" file, it will generate compositions automaticly.\n' +
+                    '**NOTE: Keep the ".jsx" file and "slice" folder under a same parent folder if you want to move it.\n' +
+                    '4.Back to Sketch,you can run Plugins -> Sketch2AE -> Clear ExportSlices to clear exsiting ExportSlices.\n' +
+                    '\n' +
+                    'Known Issues:\n' +
+                    '1.Can not export vactor shapes, editable texts, layer styles.All flatten.\n' +
+                    '2.By default it generate 1x slices.If you need 2x or other you have to export by hand.\n' +
+                    '3.Rotating or scaling of a layer will result in a big rectangle of slice\n' +
+                    '4.If there are missed fonts in the file, the ExportSlices might be clipping.\n（Try my Plungin:Font Packer to pack and achive your sketch files:) ）。\n' +
+                    '\nShortcut list:\n' +
+                    '**Becareful of shortcut conflicting with other plugins\n' +
+                    'Control + Alt + S : Generate ExportSlices\n' +
+                    'Control + Alt + E : Export Artboards\n' +
+                    'Control + Alt + C : Clear ExportSlices\n' +
+                    'Control + Alt + H : Help\n' +
+                    '\n\n' +
+                    'Any questions -> xixi@bigxixi.com\n';
+var local_updateText1 = "You are using the latest version v";
+var local_updateText2 = "You are up to date!";
+var local_updateText3 = "New verision available (v";
+var local_updateText4 = "You are using old version (v";
+var local_updateText5 = "Click OK to download the latest version.";
+var local_updateText6 = "Failed to check for new version,contact xixi@bigxixi.com for help!\n";
+var local_updateText7 = "Can not check for updates.";
---
+++ Sketch2AE.sketchplugin/Contents/Resources/Localization_zh_Hans.js
@@ -0,0 +1,41 @@
+var local_saveDoc = '请先保存文件！';
+var local_warning = '警告';
+var local_tooMuchArtboards = '本文件中画板太多，建议减少一点或者分批导出.';
+var local_pickArtboardDsp = '选择画板导出(当前页面：';
+var local_exportDsp = '如要没设置导出切片，将导出整张画板快照。';
+var local_inputLength = '输入合成时长（秒）';
+var local_inputFPS = '输入合成帧率（FPS）';
+var local_noArtboards = '当前页面没有画板:(';
+var local_done = "导出完成，是否清理'导出标记'？";
+var local_clsBtn = "清理";
+var local_notNowBtn = "暂不";
+var local_noArtboardExpoted = '未选择任何画板，什么都没导出。';
+var local_chooseTips = "请至少选择1个图层(组)。";
+var local_groupTips = "请检查所选的是否图层组。如果不是，请先将它们打组。";
+var local_helpText = '使用步骤：\n' +
+                    '1、在图层列表选中要导出的图层组（不是图层组请先打组），执行 Plugins -> Sketch2AE -> 生成‘导出切片’。\n' +
+                    '2、执行 Plugins -> Sketch2AE -> 导出文件，在弹出的面板设置合成时长和帧率并选择要导出的画板，点击OK然后选择导出地址。执行后会在该地址创建一个.jsx脚本和slice文件夹。\n' +
+                    '3、打开After Effect，执行 文件 -> 脚本 -> 运行脚本文件，选择刚才导出的.jsx文件，这样就好自动导入图片并创建合成了。\n' +
+                    '**注意，如需移动该jsx文件请带上slice文件夹并保持他们在同一父文件夹下。\n' +
+                    '4、在sketch中，如有需要，可以执行 Plugins -> Sketch2AE -> 清除‘导出切片’来清理导出标记。\n' +
+                    '\n' +
+                    '本插件局限性&下一阶段目标：\n' +
+                    '1、不支持路径、字体、图层样式等的导出。\n' +
+                    '2、目前只能自动导出1x图，如需导出多倍图请单独处理。\n' +
+                    '3、如果对图层组进行旋转、缩放后，导出的切片是变换后的矩形。\n' +
+                    '4、如果字体丢失，则导出的字体层切片可能会不全\n（硬广：请使用FontPacker插件打包字体）。\n' +
+                    '\n快捷键列表:\n' +
+                    '**如果与其他插件冲突，快捷键也许会失效，慎用\n' +
+                    'Control + Alt + S : 生成‘导出切片’\n' +
+                    'Control + Alt + E : 导出画板\n' +
+                    'Control + Alt + C : 清除‘导出切片’\n' +
+                    'Control + Alt + H : 帮助\n' +
+                    '\n\n' +
+                    '任何问题或建议欢迎联系 xixi@bigxixi.com\n';
+var local_updateText1 = "您正在使用的是最新版，版本 v";
+var local_updateText2 = "这是最新版哦";
+var local_updateText3 = "有新版本！(v";
+var local_updateText4 = "您正在使用旧版本 (v";
+var local_updateText5 = "点击 OK 下载最新版.";
+var local_updateText6 = "无法查询最新版,请联系 xixi@bigxixi.com 需求帮助！\n";
+var local_updateText7 = "更新查询失败";
